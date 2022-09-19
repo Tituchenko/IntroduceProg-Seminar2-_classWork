681e8a0a4efa42364378939ddc98fb127a0e58d5
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-19 20:25:53 +0300

    ad Ex 001 find max number in 10-99

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Случайное число от 10 до 99, вывести максимальную цифру
+int number=new Random().Next(10,100);
+int num1=number/10;
+int num2=number%10;
+
+int max=num1;
+if (max<num2) max=num2;
+Console.WriteLine ($"Максимальная цифра числа {number} это {max}");
